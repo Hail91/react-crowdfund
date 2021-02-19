776007c28594a4c2ae36d2cad5d02a2f4bd9271d
Aaron <aarongillies88@gmail.com>
2021-02-19 13:05:14 -0500

    finished basic project setup

---
+++ crowdfund/src/components/Main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+// Component imports
+import Header from "./Layout/Header.jsx";
+
+const Main = () => {
+  return (
+    <div>
+      <Header />
+      <h1>App here</h1>
+    </div>
+  );
+};
+
+export default Main;
